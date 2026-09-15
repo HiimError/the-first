a8ede1ba43a16b5e67d3ac2a350cc706b72a5a9b
Maxwell Dev <268078768+Asvyr@users.noreply.github.com>
2026-09-15 12:35:37 -0500

    Attack setup

--- Pawns/player.gd
+++ Pawns/player.gd
@@ -1,9 +1,43 @@
 extends CharacterBody2D
-@export var speed := 700.0
+
+signal player_died
 
 
+@export var speed: float = 700.0
+
+@export var max_health: float = 100
+@export var health: float = 10
+
+
+# Movement
 func _physics_process(delta: float) -> void:
+	
+	# calculate the direction and movement speed for the player
 	var input_vector = Input.get_vector("left" , "right" , "up" , "down")
 	var move_direction := input_vector.normalized()
 	velocity = speed * move_direction
+	
+	# Update attack rot, rotates the attack hitbox to face
+	# the same direction that the player is facing
+	update_attack_rot(input_vector.normalized())
 	move_and_slide()
+
+
+# this function takes in the direction the player is moving 
+# and rotates the attack box to face the same direction as the movement
+func update_attack_rot(direction: Vector2):
+	$AttackRotPoint.rotation = direction.angle()
+
+
+# this function is used to subtracting health and checking if dead
+func take_damage(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		die()
+	pass
+
+
+func die():
+	emit_signal("player_died")
+	print("Character is dead")
+	
--- game.gd
+++ game.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+
+func _player_died() -> void:
+	# respawn player
+	# reset boss health
+	pass
